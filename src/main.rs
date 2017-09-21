dfbedec68305319d0e979eb8706e7f6ceaf171b8
Maxime FRANC <mfranc@e2r7p18.42.fr>
2017-09-21 19:15:13 +0200

    doc chapter 3 : Variables

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main()
+{
+    const MAX_POINTS: u32 = 100_000;
+    let mut x = 5;
+    println!("Value of x : {}", x);
+    x = 6;
+    println!("Value of x : {}", x);
+    println!("Value of the const is : {}", MAX_POINTS);
+}
